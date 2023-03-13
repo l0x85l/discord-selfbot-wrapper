44dfe53d83da0c09d73b1e758f45587c3eb1adc9
Viserys <67641613+theviserys@users.noreply.github.com>
2023-03-14 02:58:09 +0300

    Add files via upload

---
+++ src/main/java/me/viserys/Main.java
@@ -0,0 +1,13 @@
+package me.viserys;
+
+import me.viserys.client.SelfClient;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        SelfClient selfClient = new SelfClient("");
+
+
+    }
+}
---
+++ src/main/java/me/viserys/client/impl/Avatar.java
@@ -0,0 +1,13 @@
+package me.viserys.client.impl;
+
+public class Avatar {
+    private String avatar;
+
+    public Avatar(String _avatar) {
+        avatar = _avatar;
+    }
+
+    public String avatar() {
+        return avatar;
+    }
+}
